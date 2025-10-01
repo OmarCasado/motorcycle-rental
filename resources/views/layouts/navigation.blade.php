11133b0790241901a582e371334e0e7e5754d960
Omar Casado <omarcasado27@hotmail.com>
2025-10-01 16:27:00 +0900

    ロゴを作成、追加。

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('topPage') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block h-10 w-auto fill-current text-gray-800 hover:scale-125 transition" />
                     </a>
                 </div>
 

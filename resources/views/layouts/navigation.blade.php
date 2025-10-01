bec7335ff6796cdd54795b5d869286c77d25f920
Omar Casado <omarcasado27@hotmail.com>
2025-10-01 16:01:45 +0900

    ハンバーガーメニューを修正。

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -89,19 +89,37 @@
             </div>
 
             <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                <x-responsive-nav-link :href="route('topPage')">
+                    Home
                 </x-responsive-nav-link>
 
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
+                <x-responsive-nav-link :href="route('showMyRentals')">
+                    My Rentals
+                </x-responsive-nav-link>
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                @auth
+                    @if(Auth::user()->role === 'admin')
+                    <x-responsive-nav-link :href="route('AdminShowRentals')">
+                        All Rentals
                     </x-responsive-nav-link>
+                    @endif
+                @endauth
+
+                <x-responsive-nav-link :href="route('profile.edit')">
+                    My Profile
+                </x-responsive-nav-link>
+
+                <form method="POST" action="{{ route('logout') }}" class="flex">
+                    @csrf
+                    @if(Auth::check())
+                        <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+                            Log Out
+                        </x-responsive-nav-link>
+                    @else
+                        <x-responsive-nav-link :href="route('login')">
+                            Login
+                        </x-responsive-nav-link>
+                    @endif
                 </form>
             </div>
         </div>

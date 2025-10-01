061599a0c8efbbfa9e1d6038258829233f3f981b
Omar Casado <omarcasado27@hotmail.com>
2025-10-01 15:10:56 +0900

    ナビゲーションバーにログインとログアウトを追加。

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,6 +27,24 @@
                         </x-nav-link>
                         @endif
                     @endauth
+
+                    <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
+                        My Profile
+                    </x-nav-link>
+
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        @if(Auth::check())
+                            <x-nav-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+                                Log out
+                            </x-nav-link>
+                        @else
+                            <x-nav-link :href="route('login')">
+                                Login
+                            </x-nav-link>
+                        @endif
+                    </form>
                 </div>
             </div>
 

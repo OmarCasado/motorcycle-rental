3a5fd9256d432c090130858bafa84adb575c2e14
Omar Casado <omarcasado27@hotmail.com>
2025-10-08 18:41:08 +0900

    背景動画のpositionをfixedに変更。

--- resources/views/motorcycles/indextemp.blade.php
+++ resources/views/motorcycles/indextemp.blade.php
@@ -43,7 +43,7 @@
             <a href="#" class="block text-white bg-redCustom w-[100px] p-2 rounded-lg mx-auto my-0 hover:bg-greenCustom">RESERVE</a>
         </div>
 
-        <div id="background_video" class="absolute -z-10 w-full h-full max-[900px]:hidden">
+        <div id="background_video" class="fixed -z-10 w-full h-full max-[900px]:hidden">
             <video autoplay loop muted class="w-full">
                 <source src="/videos/video_moto.mp4" type="video/mp4">
             </video>

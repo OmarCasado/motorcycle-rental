d38f37c9069d8f38655c02da130185792a217701
Omar Casado <omarcasado27@hotmail.com>
2025-10-14 15:50:18 +0900

    navigationのレスポンシブdropdownメニューの修正。

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -70,15 +70,9 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden bg-darkGray text-white z-50 absolute top-[70px] left-0 w-full p-4">
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
+        <div class="pt-4 pb-1 border-gray-200">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::check() ? Auth::user()->name : '' }}</div>
                 <div class="font-medium text-sm text-gray-500">{{Auth::check() ? Auth::user()->email: '' }}</div>
@@ -89,11 +83,11 @@
                     Home
                 </x-responsive-nav-link>
 
-                <x-nav-link :href="route('topPage')" :active="request()->routeIs('topPage')">
+                <x-responsive-nav-link :href="route('topPage')">
                     Routes
-                </x-nav-link>
+                </x-responsive-nav-link>
 
-                <x-responsive-nav-link :href="route('topPage')" :active="request()->routeIs('topPage')">
+                <x-responsive-nav-link :href="route('topPage')">
                     Prices
                 </x-responsive-nav-link>
 

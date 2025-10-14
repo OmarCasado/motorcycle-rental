c705cfa7c84de0cbc9bdc74e638e8098a4bfb823
Omar Casado <omarcasado27@hotmail.com>
2025-10-14 14:40:16 +0900

    navigationの文字色を変更。

--- resources/views/layouts/navigationTEMP.blade.php
+++ resources/views/layouts/navigationTEMP.blade.php
@@ -1,14 +1,12 @@
-<!--　ナヴィゲーション・バ－　-->
-<nav x-data="{ open: false }" id="top_nav" class="flex justify-between h-[70px] bg-darkGray fixed w-full z-10 max-[900px]:bg-transparent max-[900px]:flex max-[900px]:justify-center max-[900px]:content-center max-[900px]:flex-col max-[900px]:absolute">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="w-full mx-auto px-4 sm:px-6 lg:px-8 flex justify-between">
-        <div class="flex h-16">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
             <div class="flex">
-                <!--ロゴとブランド名を含む-->
-                <div class="shrink-0 flex justify-center">
-                    <a href="{{ route('topPage') }}" class="flex px-[10px] w-[26rem] max-w-[100vw]">
-                        <x-application-logo class="scale-105 max-w-full h-auto hover:scale-125 max-[960px]:scale-75 max-[900px]:filter-none max-[900px]:w-[25%] max-[900px]:h-auto max-[900px]:scale-[0.7]" />
-                        <h1 class="text-white hover:text-greenCustom text-2xl leading-[70px] max-[900px]:text-darkGray max-[900px]:text-[1.8rem]">J.<span class="text-redCustom">O</span>.2 Bike Rental</h1>
+                <!-- Logo -->
+                <div class="shrink-0 flex items-center">
+                    <a href="{{ route('topPage') }}">
+                        <x-application-logo class="block h-10 w-auto fill-current text-gray-800 hover:scale-125 transition" />
                     </a>
                 </div>
 
@@ -16,14 +14,6 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('topPage')" :active="request()->routeIs('topPage')">
                         Home
-                    </x-nav-link>
-
-                    <x-nav-link :href="route('topPage')">
-                        Routes
-                    </x-nav-link>
-
-                    <x-nav-link :href="route('topPage')">
-                        Prices
                     </x-nav-link>
 
                     <x-nav-link :href="route('showMyRentals')" :active="request()->routeIs('showMyRentals')">
@@ -101,14 +91,6 @@
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('topPage')">
                     Home
-                </x-responsive-nav-link>
-
-                <x-nav-link :href="route('topPage')" :active="request()->routeIs('topPage')">
-                    Routes
-                </x-nav-link>
-
-                <x-responsive-nav-link :href="route('topPage')" :active="request()->routeIs('topPage')">
-                    Prices
                 </x-responsive-nav-link>
 
                 <x-responsive-nav-link :href="route('showMyRentals')">

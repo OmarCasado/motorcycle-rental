ed88cc5ed1e4cd33185ef0384104368cae295b99
Omar Casado <omarcasado27@hotmail.com>
2025-10-24 13:44:24 +0900

    footerにtailwindスタイルを追加。

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,19 +1,19 @@
 <footer class="flex justify-between items-center bg-darkGray h-[70px]">
-    <div>
+    <div class="flex items-center gap-3">
         <small class="ml-5 text-white">© 2023 Japan On 2 Wheels @.C.H.</small>
+        <div id="media_icons" class="mr-[20px]">
+            <div class="flex">
+              <a href="#"><img src="images/icons/envelope.svg" alt="email icon" class="w-[20px] invert m-[5px_10px_7px_10px] transition-transform duration-300 hover:scale-150"></a>
+              <a href="#"><img src="images/icons/phone.svg" alt="mobile phone icon" class="w-[20px] invert m-[5px_10px_7px_10px] transition-transform duration-300 hover:scale-150"></a>
+              <a href="#"><img src="images/icons/instagram.svg" alt="instagram icon" class="w-[20px] invert m-[5px_10px_7px_10px] transition-transform duration-300 hover:scale-150"></a>
+              <a href="#"><img src="images/icons/youtube.svg" alt="youtube icon" class="w-[20px] invert m-[5px_10px_7px_10px] transition-transform duration-300 hover:scale-150"></a>
+            </div>
+        </div>
     </div>
 
     <div id="goTop" class="w-[50px] h-[50px] leading-[50px] text-center bg-redCustom rounded-[30px] mr-[20px] hover:bg-greenCustom hover:shadow-[inset_3px_3px_5px_rgb(56,58,59)] transition duration-200 ease-in">
         <a href="#">
             <i class="fa-solid fa-angles-up fa-beat-fade fa-xl text-white"></i>
         </a>
-    </div>
-
-    <div id="media_icons">
-        <h4>FOLLOW US</h4>
-        <a href="#"><img src="images/icons/envelope.svg" alt="email icon"></a>
-        <a href="#"><img src="images/icons/phone.svg" alt="mobile phone icon"></a>
-        <a href="#"><img src="images/icons/instagram.svg" alt="instagram icon"></a>
-        <a href="#"><img src="images/icons/youtube.svg" alt="youtube icon"></a>
     </div>
 </footer>

6482d400e7ed0cfad7a69f5e8e3bbd09e1e8644a
Omar Casado <omarcasado27@hotmail.com>
2025-10-28 12:10:39 +0900

    contactUsページのtailwind色全体を修正。

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -10,14 +10,14 @@
 
     <section id="prices_contact_main" class="z-10 pt-[80px] flex flex-col justify-center items-center text-center mb-[125px] bg-[url('/images/own/road3.png')] bg-center bg-cover"> <!--タイトル、パラグラフと背景動画を含むMain セクション。-->
         <div id="prices_contact_info" class="mt-[200px]">
-            <h2 class="m-0 text-[3rem] [text-shadow:2px_2px_2px_var(--dark-gray)]">GET IN TOUCH</h2>
+            <h2 class="m-0 text-[3rem] [text-shadow:2px_2px_2px_darkGray]">GET IN TOUCH</h2>
             <p class="mb-[50px]">LET US HEAR FROM YOU</p>
         </div>
     </section>
 
     <section id="contact_section" class="flex justify-between mb-[50px] max-[900px]:flex-col max-[900px]:items-center max-[900px]:justify-center">
 
-        <form action="#" method="post" class="flex flex-col justify-center items-center border border-[var(--dark-gray)] w-[40%] mx-auto p-[10px] rounded-[25px] shadow-[5px_5px_6px_lightGray] max-[900px]:w-[80%]">
+        <form action="#" method="post" class="flex flex-col justify-center items-center border border-darkGray w-[40%] mx-auto p-[10px] rounded-[25px] shadow-[5px_5px_6px_lightGray] max-[900px]:w-[80%]">
 
             <div id="input_wrapper_1" class="flex flex-col justify-center items-left w-[90%]">
                 <label for="name" class="mb-[10px]">Name:</label>
@@ -40,7 +40,7 @@
                 <textarea name="" id="message" cols="30" rows="10" placeholder="Give us your opinion / Ask us your questions." class="resize-none"></textarea>
             </div>
 
-            <input type="submit" value="Send" id="send_button" class="m-[10px] uppercase bg-[var(--red)] text-[var(--white)] w-[100px] p-[10px] rounded-[10px] hover:bg-[var(--green)] hover:shadow-[inset_3px_3px_5px_var(--dark-gray)] transition duration-200 ease-in">
+            <input type="submit" value="Send" id="send_button" class="m-[10px] uppercase bg-redCustom text-white w-[100px] p-[10px] rounded-[10px] hover:bg-greenCustom hover:shadow-[inset_3px_3px_5px_darkGray] transition duration-200 ease-in">
         </form>
 
         <div id="contact_notes" class="w-[40%] max-[900px]:w-[80%]">
@@ -54,7 +54,7 @@
                 <li class="mb-[10px]"><em>Business Hours:</em> 8:00 to 20:00, 7 days a week</li>
             </ul>
 
-            <div id="map_container" class="w-[80%] h-[50%] border border-[var(--light-gray)]">
+            <div id="map_container" class="w-[80%] h-[50%] border border-lightGray">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.1066536738886!2d135.4933756754578!3d34.70248978311734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e68d95e3a70b%3A0x1baec822e859c84a!2sOsaka%20Station!5e0!3m2!1sen!2sjp!4v1692627031264!5m2!1sen!2sjp"
                     width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"

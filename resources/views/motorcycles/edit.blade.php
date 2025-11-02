7da3f3878ab8e6fd969f387c67cb7a39b65f2b55
Omar Casado <omarcasado27@hotmail.com>
2025-11-02 19:10:34 +0900

    編集ページ　修正　編集フォームのレイアウトを修正。

--- resources/views/motorcycles/edit.blade.php
+++ resources/views/motorcycles/edit.blade.php
@@ -20,7 +20,7 @@
     <table>
         <tr>
             <td>Brand:</td>
-            <td>            <select name="brand_id" required>
+            <td> <select name="brand_id" class="px-3 leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required required>
                 @foreach($brands as $brand)
                     <option value="{{ $brand->id }}" {{ $motorcycle->brand_id == $brand->id ? 'selected' : '' }}>
                         {{ $brand->name }}
@@ -31,22 +31,22 @@
 
         <tr>
             <td>Model:</td>
-            <td><input type="text" name="model" value="{{ $motorcycle->model }}" required></td>
+            <td><input type="text" name="model" value="{{ $motorcycle->model }}" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
         </tr>
 
         <tr>
             <td>Year:</td>
-            <td><input type="number" name="year" value="{{ $motorcycle->year }}" min="2000" max="{{ date('Y') }}" required></td>
+            <td><input type="number" name="year" value="{{ $motorcycle->year }}" min="2000" max="{{ date('Y') }}" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
         </tr>
 
         <tr>
             <td>Color:</td>
-            <td><input type="text" name="color" value="{{ $motorcycle->color }}" required></td>
+            <td><input type="text" name="color" value="{{ $motorcycle->color }}" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
         </tr>
 
         <tr>
             <td>Price per Day (¥):</td>
-            <td><input type="number" name="price_per_day" value="{{ $motorcycle->price_per_day }}" min="0" required></td>
+            <td><input type="number" name="price_per_day" value="{{ $motorcycle->price_per_day }}" min="0" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
         </tr>
 
         <tr>

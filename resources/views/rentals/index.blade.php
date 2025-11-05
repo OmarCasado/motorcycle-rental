a9f1c0167bc948d9073752b1ec5d797c611b30e3
Omar Casado <omarcasado27@hotmail.com>
2025-11-05 14:49:46 +0900

    レンタル履歴ページ　修正　テーブルのスタイルを修正。

--- resources/views/rentals/index.blade.php
+++ resources/views/rentals/index.blade.php
@@ -8,31 +8,31 @@
         <p>You have no rentals yet.</p>
     @else
         <table>
-            <th class="border"ead>
+            <th>
                 <tr>
-                    <th class="border">Motorcycle</th class="border">
-                    <th class="border">Start</th class="border">
-                    <th class="border">End</th class="border">
-                    <th class="border">Total Price (¥)</th class="border">
+                    <th class="border-2 border-darkGray text-left px-2">Motorcycle</th>
+                    <th class="border-2 border-darkGray text-left px-2">Start</th>
+                    <th class="border-2 border-darkGray text-left px-2">End</th>
+                    <th class="border-2 border-darkGray text-left px-2">Total Price (¥)</th>
                 </tr>
-            </th class="border"ead>
+            </th>
             <tbody>
                 @foreach ($rentals as $rental)
                     <tr>
-                        <td class="border">{{ $rental->motorcycle->brand->name }} {{ $rental->motorcycle->model }}</td class="border">
-                        <td class="border">{{ $rental->start_datetime->format('Y-m-d H:i') }}</td class="border">
-                        <td class="border">{{ $rental->end_datetime->format('Y-m-d H:i') }}</td class="border">
-                        <td class="border">{{ number_format($rental->total_price) }}</td class="border">
-                        <td class="border">
+                        <td class="border-2 border-darkGray text-left px-2">{{ $rental->motorcycle->brand->name }} {{ $rental->motorcycle->model }}</td>
+                        <td class="border-2 border-darkGray text-left px-2">{{ $rental->start_datetime->format('Y-m-d H:i') }}</td>
+                        <td class="border-2 border-darkGray text-left px-2">{{ $rental->end_datetime->format('Y-m-d H:i') }}</td>
+                        <td class="border-2 border-darkGray text-left px-2">{{ number_format($rental->total_price) }}</td>
+                        <td>
                             @if($rental->status === 'active')
-                                <form action="{{ route('cancelMyRental', $rental->id) }}" meth class="border"od="post">
+                                <form action="{{ route('cancelMyRental', $rental->id) }}" method="post">
                                     @csrf
                                     <button type="submit">Cancel</button>
                                 </form>
                             @else
                                 <span>{{ ucfirst($rental->status) }}</span>
                             @endif
-                        </td class="border">
+                        </td>
                     </tr>
                 @endforeach
             </tbody>

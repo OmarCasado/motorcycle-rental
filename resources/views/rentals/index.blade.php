fac0d0ce15f8b3092ff3ad5b4802764985b591fb
Omar Casado <omarcasado27@hotmail.com>
2025-11-06 14:01:55 +0900

    レンタル履歴ページ　修正　テーブルの情報レイアウトを修正。

--- resources/views/rentals/index.blade.php
+++ resources/views/rentals/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="pt-[80px] h-screen flex justify-center flex-col items-center">
+<div class="pt-[80px] h-screen flex justify-center flex-col items-center max-[900px]:px-16">
     <h1 class="text-2xl mb-2">My Rentals</h1>
 
     @if ($rentals->isEmpty())
@@ -19,15 +19,15 @@
             <tbody>
                 @foreach ($rentals as $rental)
                     <tr>
-                        <td class="border-2 border-darkGray text-left px-2">{{ $rental->motorcycle->brand->name }} {{ $rental->motorcycle->model }}</td>
-                        <td class="border-2 border-darkGray text-left px-2">{{ $rental->start_datetime->format('Y-m-d H:i') }}</td>
-                        <td class="border-2 border-darkGray text-left px-2">{{ $rental->end_datetime->format('Y-m-d H:i') }}</td>
-                        <td class="border-2 border-darkGray text-left px-2">{{ number_format($rental->total_price) }}</td>
+                        <td class="border-2 border-darkGray text-left px-2 max-[900px]:text-sm">{{ $rental->motorcycle->brand->name }} {{ $rental->motorcycle->model }}</td>
+                        <td class="border-2 border-darkGray text-left px-2 max-[900px]:text-sm">{{ $rental->start_datetime->format('Y/m/d H:i') }}</td>
+                        <td class="border-2 border-darkGray text-left px-2 max-[900px]:text-sm">{{ $rental->end_datetime->format('Y/m/d H:i') }}</td>
+                        <td class="border-2 border-darkGray text-left px-2 max-[900px]:text-sm">{{ number_format($rental->total_price) }}</td>
                         <td>
                             @if($rental->status === 'active')
                                 <form action="{{ route('cancelMyRental', $rental->id) }}" method="post">
                                     @csrf
-                                    <button type="submit" class="btn btn-red">Cancel</button>
+                                    <button type="submit" class="btn btn-red ms-2">Cancel</button>
                                 </form>
                             @else
                                 <span>{{ ucfirst($rental->status) }}</span>

d733a4f21221a9677ff7b37fe2d30513939e0681
Omar Casado <omarcasado27@hotmail.com>
2025-11-13 11:46:36 +0900

    管理者レンタル画面 削除　不要なクラスを削除。

--- resources/views/admin/rentals/index.blade.php
+++ resources/views/admin/rentals/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="pt-[80px] h-screen flex justify-center flex-col items-center max-[900px]:px-16">
+<div class="pt-[80px] h-screen flex justify-center flex-col items-center max-[900px]:px-16 overflow-x-auto">
     <h1 class="text-2xl mb-2">All Rentals (Admin Dashboard)</h1>
 
     @if ($rentals->isEmpty())
@@ -10,23 +10,23 @@
         <table>
             <thead>
                 <tr>
-                    <th class="border-2 border-darkGray text-left px-2 max-[900px]:text-sm max-[900px]:px-1">User</th>
-                    <th class="border-2 border-darkGray text-left px-2 max-[900px]:text-sm max-[900px]:px-1">Motorcycle</th>
-                    <th class="border-2 border-darkGray text-left px-2 max-[900px]:text-sm max-[900px]:px-1">Start</th>
-                    <th class="border-2 border-darkGray text-left px-2 max-[900px]:text-sm max-[900px]:px-1">End</th>
-                    <th class="border-2 border-darkGray text-left px-2 max-[900px]:text-sm max-[900px]:px-1">Total Price (¥)</th>
-                    <th class="border-2 border-darkGray text-left px-2 max-[900px]:text-sm max-[900px]:px-1">Status</th>
+                    <th class="border-2 border-darkGray text-left px-2">User</th>
+                    <th class="border-2 border-darkGray text-left px-2">Motorcycle</th>
+                    <th class="border-2 border-darkGray text-left px-2">Start</th>
+                    <th class="border-2 border-darkGray text-left px-2">End</th>
+                    <th class="border-2 border-darkGray text-left px-2">Total Price (¥)</th>
+                    <th class="border-2 border-darkGray text-left px-2">Status</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($rentals as $rental)
                     <tr>
-                        <td class="border-2 border-darkGray text-left px-5 max-[900px]:text-sm max-[900px]:px-2">{{ $rental->user->name }} ({{ $rental->user->email }})</td>
-                        <td class="border-2 border-darkGray text-left px-5 max-[900px]:text-sm max-[900px]:px-2">{{ $rental->motorcycle->brand->name }} {{ $rental->motorcycle->model }}</td>
-                        <td class="border-2 border-darkGray text-left px-5 max-[900px]:text-sm max-[900px]:px-2">{{ $rental->start_datetime->format('Y/m/d H:i') }}</td>
-                        <td class="border-2 border-darkGray text-left px-5 max-[900px]:text-sm max-[900px]:px-2">{{ $rental->end_datetime->format('Y/m/d H:i') }}</td>
-                        <td class="border-2 border-darkGray text-left px-5 max-[900px]:text-sm max-[900px]:px-2">{{ number_format($rental->total_price) }}</td>
-                        <td class="border-2 border-darkGray text-left px-5 max-[900px]:text-sm max-[900px]:px-2">{{ ucfirst($rental->status) }}</td>
+                        <td class="border-2 border-darkGray text-left px-5">{{ $rental->user->name }} ({{ $rental->user->email }})</td>
+                        <td class="border-2 border-darkGray text-left px-5">{{ $rental->motorcycle->brand->name }} {{ $rental->motorcycle->model }}</td>
+                        <td class="border-2 border-darkGray text-left px-5">{{ $rental->start_datetime->format('Y/m/d H:i') }}</td>
+                        <td class="border-2 border-darkGray text-left px-5">{{ $rental->end_datetime->format('Y/m/d H:i') }}</td>
+                        <td class="border-2 border-darkGray text-left px-5">{{ number_format($rental->total_price) }}</td>
+                        <td class="border-2 border-darkGray text-left px-5">{{ ucfirst($rental->status) }}</td>
                         <td>
                             @if ($rental->status === 'active')
                                 <form action="{{ route('cancelMyRental', $rental->id) }}" method="POST" style="display:inline;">

311104a82d218ab0380ac694419a109aaefea197
Omar Casado <omarcasado27@hotmail.com>
2025-11-14 14:40:31 +0900

    管理者レンタル画面 修正　テ－ブルを携帯画面で非表示。

--- resources/views/admin/rentals/index.blade.php
+++ resources/views/admin/rentals/index.blade.php
@@ -10,40 +10,43 @@
         {{-- モバイル用のカード表示 --}}
 
         {{-- デスクトップ用のテーブル表示 --}}
-        <table>
-            <thead>
-                <tr>
-                    <th class="border-2 border-darkGray text-left px-2">User</th>
-                    <th class="border-2 border-darkGray text-left px-2">Motorcycle</th>
-                    <th class="border-2 border-darkGray text-left px-2">Start</th>
-                    <th class="border-2 border-darkGray text-left px-2">End</th>
-                    <th class="border-2 border-darkGray text-left px-2">Total Price (¥)</th>
-                    <th class="border-2 border-darkGray text-left px-2">Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($rentals as $rental)
+        <div class="hidden sm:block overflow-x-auto">
+            <table class="min-w-max table-auto border-collapse">
+                <thead>
                     <tr>
-                        <td class="border-2 border-darkGray text-left px-5">{{ $rental->user->name }} ({{ $rental->user->email }})</td>
-                        <td class="border-2 border-darkGray text-left px-5">{{ $rental->motorcycle->brand->name }} {{ $rental->motorcycle->model }}</td>
-                        <td class="border-2 border-darkGray text-left px-5">{{ $rental->start_datetime->format('Y/m/d H:i') }}</td>
-                        <td class="border-2 border-darkGray text-left px-5">{{ $rental->end_datetime->format('Y/m/d H:i') }}</td>
-                        <td class="border-2 border-darkGray text-left px-5">{{ number_format($rental->total_price) }}</td>
-                        <td class="border-2 border-darkGray text-left px-5">{{ ucfirst($rental->status) }}</td>
-                        <td>
-                            @if ($rental->status === 'active')
-                                <form action="{{ route('cancelMyRental', $rental->id) }}" method="POST" style="display:inline;">
-                                    @csrf
-                                    <button type="submit" class="btn btn-red ms-2">Cancel</button>
-                                </form>
-                            @else
-                                <span>-</span>
-                            @endif
-                        </td>
+                        <th class="border-2 border-darkGray text-left px-2">User</th>
+                        <th class="border-2 border-darkGray text-left px-2">Motorcycle</th>
+                        <th class="border-2 border-darkGray text-left px-2">Start</th>
+                        <th class="border-2 border-darkGray text-left px-2">End</th>
+                        <th class="border-2 border-darkGray text-left px-2">Total Price (¥)</th>
+                        <th class="border-2 border-darkGray text-left px-2">Status</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($rentals as $rental)
+                        <tr>
+                            <td class="border-2 border-darkGray text-left px-5">{{ $rental->user->name }} ({{ $rental->user->email }})</td>
+                            <td class="border-2 border-darkGray text-left px-5">{{ $rental->motorcycle->brand->name }} {{ $rental->motorcycle->model }}</td>
+                            <td class="border-2 border-darkGray text-left px-5">{{ $rental->start_datetime->format('Y/m/d H:i') }}</td>
+                            <td class="border-2 border-darkGray text-left px-5">{{ $rental->end_datetime->format('Y/m/d H:i') }}</td>
+                            <td class="border-2 border-darkGray text-left px-5">{{ number_format($rental->total_price) }}</td>
+                            <td class="border-2 border-darkGray text-left px-5">{{ ucfirst($rental->status) }}</td>
+                            <td>
+                                @if ($rental->status === 'active')
+                                    <form action="{{ route('cancelMyRental', $rental->id) }}" method="POST" style="display:inline;">
+                                        @csrf
+                                        <button type="submit" class="btn btn-red ms-2">Cancel</button>
+                                    </form>
+                                @else
+                                    <span>-</span>
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     @endif
+
 </div>
 @endsection

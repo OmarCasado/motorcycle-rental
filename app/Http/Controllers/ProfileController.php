62a77e23d88bdf19944ab65ea63775c2a1fe1d4d
Omar Casado <omarcasado27@hotmail.com>
2025-11-21 14:22:54 +0900

    マイプロフィールページ 追加　ProfileControllerにRentalモデルを追加。

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Rental;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;

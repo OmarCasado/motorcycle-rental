f556bd910f5ee2563c90bb2b21dd4791bd2af2d8
Omar Casado <omarcasado27@hotmail.com>
2025-11-22 12:39:03 +0900

    マイプロフィールページ 修正　ブレ－ドでボタンのルートを修正。

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -46,7 +46,7 @@
             </dl>
 
 
-            <a href="{{ route('showMyProfile') }}"
+            <a href="{{ route('editMyProfile') }}"
                class="mt-5 inline-block rounded-[10px] bg-greenCustom px-4 py-2 text-sm font-medium text-whiteCustom hover:shadow-[inset_3px_3px_5px_rgb(56,58,59)] transition duration-200 ease-in">
                 Edit profile
             </a>

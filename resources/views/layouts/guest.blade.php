ff235926a70f8325138c19f690abed4358500ab8
Omar Casado <omarcasado27@hotmail.com>
2025-11-25 13:35:42 +0900

    ログインページ　追加　トップバナーを追加。

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,6 +15,11 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
+        <!-- Page Heading -->
+        <header>
+            @include('layouts.navigation')
+        </header>
+
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">

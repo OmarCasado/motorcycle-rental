f7ce9644ae9f592f111096ec1287a2053a1274f3
Omar Casado <omarcasado27@hotmail.com>
2025-11-27 12:19:02 +0900

    トップページ　修正　コメントの書き換え。

--- resources/views/motorcycles/index.blade.php
+++ resources/views/motorcycles/index.blade.php
@@ -37,7 +37,7 @@
             </a></li>
     </ul>
 </aside>
-<!-- 新しいモデルを提示するセクション。各モデルは単独のarticleで構成する。-->
+<!-- レンタル可能バイクを提示するセクション。各モデルは単独のarticleで構成する。-->
 <section id="new_models" class="text-center bg-darkGray text-white h-screen max-[900px]:h-auto max-[900px]:pb-12">
     <h2 id="new_models_title" class="m-0 text-5xl pt-12 mb-12 [text-shadow:2px_2px_2px_rgb(56,58,59)] max-[900px]:text-2xl"> RIDE OUR NEW MODELS</h2>
     {{-- Cards Grid --}}

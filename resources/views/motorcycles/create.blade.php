d81f80e347943fc424db6c1272523484fd23a70b
Omar Casado <omarcasado27@hotmail.com>
2025-11-27 12:50:22 +0900

    バイクを追加ページ　修正　ブレ－ドのタグレイアウトを修正。

--- resources/views/motorcycles/create.blade.php
+++ resources/views/motorcycles/create.blade.php
@@ -20,46 +20,45 @@
     <div class="max-[900px]:pl-[100px]">
         <table>
             <tr>
-            <td>Brand:</td>
-            <td> <select name="brand_id" class="px-3 leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required>
-                @foreach($brands as $brand)
-                    <option value="{{ $brand->id }}">{{ $brand->name }}</option>
-                @endforeach
-            </select></td>
-        </tr>
+                <td>Brand:</td>
+                <td> <select name="brand_id" class="px-3 leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required>
+                    @foreach($brands as $brand)
+                        <option value="{{ $brand->id }}">{{ $brand->name }}</option>
+                    @endforeach
+                </select></td>
+            </tr>
 
-        <tr>
-            <td>Model:</td>
-            <td><input type="text" name="model" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
-        </tr>
+            <tr>
+                <td>Model:</td>
+                <td><input type="text" name="model" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
+            </tr>
 
-        <tr>
-            <td>Year:</td>
-            <td><input type="number" name="year" min="2000" max="{{ date('Y') }}" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
-        </tr>
+            <tr>
+                <td>Year:</td>
+                <td><input type="number" name="year" min="2000" max="{{ date('Y') }}" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
+            </tr>
 
-        <tr>
-            <td>Color:</td>
-            <td><input type="text" name="color" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
-        </tr>
+            <tr>
+                <td>Color:</td>
+                <td><input type="text" name="color" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
+            </tr>
 
-        <tr>
-            <td>Price per Day (¥):</td>
-            <td><input type="number" name="price_per_day" min="0" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
-        </tr>
+            <tr>
+                <td>Price per Day (¥):</td>
+                <td><input type="number" name="price_per_day" min="0" class="typed_input leading-[1.5rem] my-[5px] rounded-[5px] border border-darkGray py-0" required></td>
+            </tr>
 
-        <tr>
-            <td>Motorcycle Image:</td>
-            <td><input type="file" name="image" id="image"></td>
-        </tr>
+            <tr>
+                <td>Motorcycle Image:</td>
+                <td><input type="file" name="image" id="image"></td>
+            </tr>
 
-        <tr>
-            <td>Available:</td>
-            <td><input type="checkbox" name="is_available"></td>
-        </tr>
+            <tr>
+                <td>Available:</td>
+                <td><input type="checkbox" name="is_available"></td>
+            </tr>
         </table>
     </div>
-
 
     <button type="submit" class="btn btn-red">Save</button>
     </form>

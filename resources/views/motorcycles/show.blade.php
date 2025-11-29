063e3d2cdd0c58a157301f0d858380fe66cc6d05
Omar Casado <omarcasado27@hotmail.com>
2025-11-29 10:34:03 +0900

    詳細ページ　修正　レンタル成功メッセージのスタイルを修正。

--- resources/views/motorcycles/show.blade.php
+++ resources/views/motorcycles/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 @if (session('success'))
-    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4 pt-[90px]">
+    <div class="text-green-700 px-4 py-3 rounded mb-4 pt-[90px]">
         {{ session('success') }}
     </div>
 @endif

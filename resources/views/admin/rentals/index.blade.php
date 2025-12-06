98faaa676780e70279037de6388851c51fd2e410
Omar Casado <omarcasado27@hotmail.com>
2025-12-06 19:55:10 +0900

    管理者レンタル画面　削除　不要な文章を削除。

--- resources/views/admin/rentals/index.blade.php
+++ resources/views/admin/rentals/index.blade.php
@@ -50,8 +50,6 @@
                                 @csrf
                                 <button type="submit" class="btn btn-red">Cancel</button>
                             </form>
-                        @else
-                            <span class="text-sm text-darkGray/70">—</span>
                         @endif
                     </div>
                 </div>

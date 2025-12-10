fc8d43660d14d91aa3f0bdf9f0dc2c75b14afa1d
Omar Casado <omarcasado27@hotmail.com>
2025-12-10 17:49:25 +0900

    共通　追加　ReviewControllerにメソッドを追加。

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -7,9 +7,24 @@ use Illuminate\Http\Request;
 class ReviewController extends Controller
 {
     /**
+     * レヴュー一覧を表示
+     */
+    public function index(){
+        //
+    }
+
+    /**
      * レヴューを保存
      */
     public function store()
+    {
+        //
+    }
+
+    /**
+     * レヴューを削除
+     */
+    public function delete()
     {
         //
     }

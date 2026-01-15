0a72ad166a337701d22d9f14281f84a34870b617
Omar Casado <omarcasado27@hotmail.com>
2026-01-15 12:28:48 +0900

    共通　修正　MotorcycleSeeder.php のバイクを修正、イメージを追加。

--- database/seeders/MotorcycleSeeder.php
+++ database/seeders/MotorcycleSeeder.php
@@ -13,33 +13,36 @@ class MotorcycleSeeder extends Seeder
     public function run(): void
     {
         $brandIds = DB::table('brands')
-            ->whereIn('name', ['Honda', 'Yamaha', 'Kawasaki'])
+            ->whereIn('name', ['Harley-Davidson', 'Kawasaki', 'Yamaha'])
             ->pluck('id', 'name');
 
         DB::table('motorcycles')->insert([
             [
-                'brand_id'      => $brandIds['Honda'],
-                'model'         => 'CB400 Super Four',
+                'brand_id'      => $brandIds['Harley-Davidson'],
+                'model'         => 'LiveWire',
                 'year'          => 2020,
-                'color'         => 'Black',
-                'price_per_day' => 4500,
+                'color'         => 'Orange',
+                'price_per_day' => 10000,
                 'is_available'  => true,
-            ],
-            [
-                'brand_id'      => $brandIds['Yamaha'],
-                'model'         => 'MT-07',
-                'year'          => 2022,
-                'color'         => 'Blue',
-                'price_per_day' => 5500,
-                'is_available'  => true,
+                'image_path'    => 'images/o-dan/o-dan_harley_livewire_transparent.png',
             ],
             [
                 'brand_id'      => $brandIds['Kawasaki'],
                 'model'         => 'Ninja 400',
-                'year'          => 2021,
+                'year'          => 2022,
                 'color'         => 'Green',
+                'price_per_day' => 5500,
+                'is_available'  => true,
+                'image_path' => 'images/o-dan/o-dan_kawasaki_transparent.png',
+            ],
+            [
+                'brand_id'      => $brandIds['Yamaha'],
+                'model'         => 'R3',
+                'year'          => 2021,
+                'color'         => 'Blue',
                 'price_per_day' => 6000,
                 'is_available'  => false,
+                'image_path' => 'images/o-dan/o-dan_yamaha_r3_transparent.png',
             ],
         ]);
     }
